4d32c9b373cde3eb15bed4831de9a4157306f0b7
stian126jg-svg <stian1.26jg@gmail.com>
2026-05-28 14:54:13 +0200

    Fun with C#

--- Program.cs
+++ Program.cs
@@ -1,26 +1,24 @@
-﻿// Collections (objects, lists, arrays and such structures) in C#
+﻿
+
+
+
+
+// Collections (objects, lists, arrays and such structures) in C#
 // In JS we could
 // let todolist = ["task", "another task", "osv"]
 
-// In C#
+// Loops in (forof array)
+// forof (let element of array) etc...
+
+
+// Console based to-do list app (CLI-app) Command Line Interface
+
+// Data related things
 List<string> todolist = new List<string>();
 
 todolist.Add("Eat");
 todolist.Add("Sleep");
 todolist.Add("Code");
-
-// Loops in (forof array)
-// forof (let element of array) etc...
-
-// In C#
-foreach (var Task in todolist)
-{
-    Console.WriteLine(Task);
-}
-
-
-
-// Console based to-do list app (CLI-app) Command Line Interface
 
 // I. Show user initial UI menu
 // To-Do list menu:
@@ -29,12 +27,6 @@ foreach (var Task in todolist)
 // 3. Remove a Task
 // 4. Exit interface
 
-Console.WriteLine("To-Do list menu");
-
-Console.WriteLine("1. Add task");
-Console.WriteLine("2. List all tasks");
-Console.WriteLine("3. Remove a task");
-Console.WriteLine("4. Exit interface");
 
 
 bool running = true;
@@ -42,19 +34,29 @@ bool running = true;
 // II. Await user input (in an infinite loop)
 while(running)
 {
+Console.WriteLine("To-Do list menu");
+
+Console.WriteLine("1. Add task");
+Console.WriteLine("2. List all tasks");
+Console.WriteLine("3. Remove a task");
+Console.WriteLine("4. Exit interface");
     
 string choice = Console.ReadLine();
 
 Console.WriteLine("User chose: " + choice);
 
-    if (choice == "1")
+    if (choice == "1") // 1. Add task
     {
-        
+        Console.WriteLine("Enter your task: ");
+        string newTodo = Console.ReadLine();
     }
 
-    else if (choice == "2")
+    else if (choice == "2") // 2. List all tasks
     {
-        
+        foreach (var Task in todolist)
+        {
+            Console.WriteLine(Task);
+        }
     }
 
     else if (choice == "3")
