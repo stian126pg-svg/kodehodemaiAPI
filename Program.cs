661c9fae18f3da09bb448b5f6d039cd060060629
stian126jg-svg <stian1.26jg@gmail.com>
2026-05-28 13:58:54 +0200

    if & indentation

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
 ﻿// Console based to-do list app (CLI-app) Command Line Interface
+
+// I. Show user initial UI menu
+// To-Do list menu:
+// 1. Add Task
+// 2. List all Tasks
+// 3. Remove a Task
+// 4. Exit interface
+
+Console.WriteLine("To-Do list menu");
+
+Console.WriteLine("1. Add task");
+Console.WriteLine("2. List all tasks");
+Console.WriteLine("3. Remove a task");
+Console.WriteLine("4. Exit interface");
+
+// II. Await user input
+string choice = Console.ReadLine();
+
+Console.WriteLine("User chose: " + choice);
+
+if (true)
+{ // Curly brackets must be on a new line, or else .net developers will be SEVERELY disappointed. And so will I.
+
+// Nothing here
+    // Code here
+}
